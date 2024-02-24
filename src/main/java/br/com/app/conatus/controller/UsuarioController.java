4a60a35ef6941a9b693b810db936077475218290
jonassantos1000 <jonasvale3@hotmail.com>
2024-02-23 23:13:27 -0300

    fix: ajusta redirecionamento apos login

--- src/main/java/br/com/app/conatus/controller/UsuarioController.java
+++ src/main/java/br/com/app/conatus/controller/UsuarioController.java
@@ -1,13 +1,13 @@
 package br.com.app.conatus.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -16,18 +16,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UsuarioController {
 
-	private final OAuth2AuthorizedClientService clientService;
 	
 	@Value("${conatus.oauth2.callback.url}")
 	private String URL_BASE;
 
 	@GetMapping()
-	public void integrarLoginGoogle(OAuth2AuthenticationToken oAuth2AuthenticationToken, HttpServletResponse response) throws Exception {
+	public void integrarLoginGoogle(OAuth2AuthenticationToken oAuth2AuthenticationToken, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		String idToken = ((DefaultOidcUser) oAuth2AuthenticationToken.getPrincipal()).getIdToken().getTokenValue();
-		
-		response.addHeader("Authorization", "Bearer ".concat(idToken));
-		response.sendRedirect(URL_BASE.concat("/login"));
+
+
+		response.sendRedirect(URL_BASE.concat("/login").concat("?token=").concat(idToken));
+
+
 	}
 	
 }
